2fdf7af697e769e9393fd81d493f9bf21c3bbedd
Keyi Zhang <keyi@stanford.edu>
2020-06-28 17:58:32 -0700

    add tex to svg/pdf build system

---
+++ images/svg.mk
@@ -0,0 +1,16 @@
+TEX=pdflatex
+TEX_FILES=$(shell find *.tex)
+SVG_FILES=$(TEX_FILES:.tex=.svg)
+
+.PRECIOUS: %.pdf
+
+all: $(SVG_FILES)
+
+%.pdf: %.tex
+	$(TEX) $<
+
+%.svg: %.pdf
+	pdf2svg $< $@
+
+clean:
+	rm -rf *.svg *.pdf
