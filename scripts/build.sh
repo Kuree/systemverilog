efd5a1b5eb326a75ca1278dc47588cfeab52fdfb
Keyi Zhang <keyi@stanford.edu>
2020-06-28 18:00:48 -0700

    add build command

---
+++ scripts/build.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+# get the file directory
+FILE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+ROOT_DIR=$(dirname ${FILE_DIR})
+
+cd ${ROOT_DIR}
+make -C images/
+make systemverilog.pdf
