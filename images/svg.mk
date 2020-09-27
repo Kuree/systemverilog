e5989849a8933ecccb39aeb7cb83fd4e2c0fd08f
Keyi Zhang <keyi@stanford.edu>
2020-09-27 16:04:11 -0700

    fix image build makefile that works with any dirs

--- images/svg.mk
+++ images/svg.mk
@@ -1,6 +1,7 @@
 TEX=pdflatex
 TEX_FILES=$(shell find *.tex)
-SVG_FILES=$(TEX_FILES:.tex=.svg)
+DOT_FILES=$(shell find *.dot)
+SVG_FILES=$(TEX_FILES:.tex=.svg) $(DOT_FILES:.dot=.svg)
 
 .PRECIOUS: %.pdf
 
@@ -8,6 +9,9 @@ all: $(SVG_FILES)
 
 %.pdf: %.tex
 	$(TEX) $<
+
+%.pdf: %.dot
+	dot -Tpdf $< > $@
 
 %.svg: %.pdf
 	pdf2svg $< $@
